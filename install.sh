6ffd32e418a2671e1417d77b2dec339d5cded12f
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-03-04 14:42:28 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Installing..."
 
-pip3 install -r requirements.txt
-python3 -m spacy download en_core_web_sm
-
-echo "Done!"
+pip3 install -r requirements.txt && echo "Installed packages successfully!" || echo "Failed to install packages!"
+python3 -m spacy download en_core_web_sm && echo "Installed needed Spacy AI models!\n\nDone!" || echo "Failed to install needed Spacy AI models!"
