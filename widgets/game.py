e62f823fe16a8510dcc277ef950aefcb343a82b6
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-03-11 16:46:19 +1100

    added a keyboard binding to open a game

--- widgets/game.py
+++ widgets/game.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 
 from textual.widgets import Label, Static
 from textual.containers import Vertical, Horizontal
+from textual import events, on
+from textual.binding import Binding
 
 
 @dataclass
@@ -32,7 +34,15 @@ class Game(Vertical, can_focus=True, can_focus_children=False):
         &.-hover { opacity: 1; }
     }
     """
-    
+
+    BINDINGS = [
+        Binding(
+            "enter",
+            "open_game",
+            "open game",
+            tooltip="Open the game you have selected"
+        )
+    ]    
 
     def __init__(self, game_info: GameInfo):
         self.game_info = game_info
@@ -43,3 +53,12 @@ class Game(Vertical, can_focus=True, can_focus_children=False):
         with Horizontal(classes="header"):
             yield Label(info.title, id="title")
         yield Static(info.description, classes="description")
+
+    @on(events.Enter)
+    @on(events.Leave)
+    def on_enter(self, event):
+        event.stop()
+        self.set_class(self.is_mouse_over, "-hover")
+
+    def action_open_game(self):
+        self.app.notify("You openned the game!!!!!")
