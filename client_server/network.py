3f5bebac9825f6a33db1c4bba5ca48cfa1e7e185
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-03-15 16:47:05 +1100

    why is port forwarding so hard?

--- client_server/network.py
+++ client_server/network.py
@@ -6,7 +6,7 @@ class Network:
     def __init__(self, server_ip: str):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = server_ip
-        self.port = 5555
+        self.port = 2222
         self.addr = (self.server, self.port)
         self.player = self.connect()
 
