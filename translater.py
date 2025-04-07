995c7b9d027dd03dec3e0b8d88f120047d20f0a3
SpookyDervish <nchand05@outlook.com>
2025-04-07 12:28:24 +1000

    tryna fix it god damnit

--- translater.py
+++ translater.py
@@ -284,7 +284,7 @@ def get_ipa_pronounciation(gorgus: str):
         "ɹs": ["rse"],
         "aɪk": ["ike"],
         "aɪd": ["ide", "ied"],
-        "oʃia": ["oćhia"],
+        "gɔɹgoʃia": ["gorgoćhia"],
         "": ['a̱', 'ḇ', 'c̱', 'ḏ', 'e̱', 'f̱', 'g̱', 'ẖ', 'i̱', 'j̱', 'ḵ', 'ḻ', 'm̱', 'ṉ', 'o̱', 'p̱', 'q̱', 'ṟ', 's̱', 'ṯ', 'u̱', 'v̱', 'w̱', 'x̱', 'y̱', 'ẕ'] # silent letters
     }
 
