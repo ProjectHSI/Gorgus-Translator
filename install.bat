a738ac74baba0448d169bab11b3202ae08987fc9
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-04-07 18:46:32 +1000

    install fix

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
 @echo off
 echo Installing...
 
+pip install -U pip setuptools wheel
+pip install -U 'spacy[cuda11x]'
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
 
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Installing..."
 
+pip3 install -U pip setuptools wheel
+pip3 install -U 'spacy[apple]'
 pip3 install -r requirements.txt && echo "Installed packages successfully!" || echo "Failed to install packages!"
 python3 -m spacy download en_core_web_sm && echo "Installed needed Spacy AI models!\n\nDone!" || echo "Failed to install needed Spacy AI models!"
