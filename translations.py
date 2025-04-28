040369521eb25f87e1381c1b6ecb643f6f6fd5c4
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-04-29 07:43:20 +1000

    aaasdasdasdasd

--- translations.py
+++ translations.py
@@ -397,7 +397,7 @@ translation_dictionary = {
     "wushab": "spread",
     'yaẖç': "chase",
     "binç": "born",
-    "çaç": "strangle",
+    "çaç": ["choke", "strangle"],
 
     # Proper Nouns
     "googrung": ["you", "thee"],
