41405fec821d6854e61e10e66e60b360160cdcd1
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-05-05 16:00:18 +1000

    added averaged_perceptron_tagger_eng to the auto downloader

--- translater.py
+++ translater.py
@@ -61,6 +61,7 @@ def nltk_download(packagePath, package) -> bool:
 wordnet_download_success = nltk_download("corpora/wordnet.zip", "wordnet")
 brown_download_success = nltk_download("corpora/brown.zip", "brown")
 brown_download_success = nltk_download("tokenizers/punkt_tab.zip", "punkt_tab")
+brown_download_success = nltk_download("taggers/averaged_perceptron_tagger_eng.zip", "averaged_perceptron_tagger_eng")
 if not (wordnet_download_success and brown_download_success):
     raise Exception("WordNet Download Failed!")
 import nltk.corpus
